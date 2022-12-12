f674c84dd0de092a1130748917951d035b468a64
pezzu <7243796+pezzu@users.noreply.github.com>
2022-12-11 19:58:32 -0500

    Some code clean up for Day 9

--- 09/index.js
+++ 09/index.js
@@ -1,97 +1,71 @@
 const fs = require("fs");
 
-const motions = fs
+const input = fs
   .readFileSync(__dirname + "/input.txt", "utf8")
   // .readFileSync(__dirname + "/test.txt", "utf8")
-  .split("\r\n")
-  .map((line) => line.split(" "));
-
-// let headPos = [0, 0];
-// let tailPos = [0, 0];
+  .split("\r\n");
 
 let visited = new Set();
 visited.add([0, 0].join(","));
 
 let rope = Array.from({ length: 10 }, () => [0, 0]);
 
-// motions.forEach((motion) => {
-//   const direction = motion[0];
-//   const value = parseInt(motion[1]);
+const parse = (line) => {
+  const [direction, distance] = line.split(" ");
+  return [direction, parseInt(distance)];
+};
 
-//   switch (direction) {
-//     case "U":
-//       headPos[1] += value;
-//       break;
-//     case "D":
-//       headPos[1] -= value;
-//       break;
-//     case "L":
-//       headPos[0] -= value;
-//       break;
-//     case "R":
-//       headPos[0] += value;
-//       break;
-//   }
+const toSteps = ([direction, distance]) => {
+  const steps = Array.from({ length: distance });
+  switch (direction) {
+    case "U":
+      return steps.map(() => [0, 1]);
+    case "D":
+      return steps.map(() => [0, -1]);
+    case "L":
+      return steps.map(() => [-1, 0]);
+    case "R":
+      return steps.map(() => [1, 0]);
+  }
+};
 
-//   updateNextKnot(headPos, tailPos);
-// });
+const visit = (rope) => {
+  visited.add(rope[rope.length - 1].join(","));
+};
 
-function updateNextKnot(headPos, tailPos, visit = false) {
-  const xDiff = headPos[0] - tailPos[0];
-  const yDiff = headPos[1] - tailPos[1];
+const moveHead = (rope, step) => {
+  rope[0] = [rope[0][0] + step[0], rope[0][1] + step[1]];
+};
+
+const pullRest = (rope) =>
+  rope.forEach((knot, i, rope) => {
+    if (i === 0) return;
+    pull(rope[i - 1], knot);
+  });
+
+const move = (rope, step) => {
+  moveHead(rope, step);
+  pullRest(rope);
+};
+
+const pull = (head, tail) => {
+  const xDiff = head[0] - tail[0];
+  const yDiff = head[1] - tail[1];
   if (xDiff === 0) {
-    for (let i = 0; i < Math.abs(yDiff) - 1; i++) {
-      tailPos[1] += Math.sign(yDiff);
-      if (visit) visited.add([tailPos[0], tailPos[1]].join(","));
-    }
+    tail[1] += Math.sign(yDiff) * (Math.abs(yDiff) - 1);
   } else if (yDiff === 0) {
-    for (let i = 0; i < Math.abs(xDiff) - 1; i++) {
-      tailPos[0] += Math.sign(xDiff);
-      if (visit) visited.add([tailPos[0], tailPos[1]].join(","));
-    }
-  } else if (Math.abs(xDiff) > 1) {
-    tailPos[0] += Math.sign(xDiff);
-    tailPos[1] += Math.sign(yDiff);
-    if (visit) visited.add([tailPos[0], tailPos[1]].join(","));
-    for (let i = 1; i < Math.abs(xDiff) - 1; i++) {
-      tailPos[0] += Math.sign(xDiff);
-      if (visit) visited.add([tailPos[0], tailPos[1]].join(","));
-    }
-  } else if (Math.abs(yDiff) > 1) {
-    tailPos[0] += Math.sign(xDiff);
-    tailPos[1] += Math.sign(yDiff);
-    if (visit) visited.add([tailPos[0], tailPos[1]].join(","));
-    for (let i = 1; i < Math.abs(yDiff) - 1; i++) {
-      tailPos[1] += Math.sign(yDiff);
-      if (visit) visited.add([tailPos[0], tailPos[1]].join(","));
-    }
+    tail[0] += Math.sign(xDiff) * (Math.abs(xDiff) - 1);
+  } else if (Math.abs(xDiff) > 1 || Math.abs(yDiff) > 1) {
+    tail[0] += Math.sign(xDiff);
+    tail[1] += Math.sign(yDiff);
   }
-}
+};
 
-motions.forEach((motion) => {
-  const direction = motion[0];
-  const value = parseInt(motion[1]);
+const steps = input.map(parse).map(toSteps).flat();
 
-  for (let i = 0; i < value; i++) {
-    switch (direction) {
-      case "U":
-        rope[0][1] += 1;
-        break;
-      case "D":
-        rope[0][1] -= 1;
-        break;
-      case "L":
-        rope[0][0] -= 1;
-        break;
-      case "R":
-        rope[0][0] += 1;
-        break;
-    }
-
-    for (let j = 0; j < rope.length - 1; j++) {
-      updateNextKnot(rope[j], rope[j + 1], j + 1 === rope.length - 1);
-    }
-  }
+steps.forEach((step) => {
+  move(rope, step);
+  visit(rope);
 });
 
 console.log(visited.size);
