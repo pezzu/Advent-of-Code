d49aea6ce1c1c18142419858f4011358e63f28c3
pezzu <7243796+pezzu@users.noreply.github.com>
2022-12-15 13:20:32 -0500

    Day 15 (part 1,2)

--- 15/index.js
+++ 15/index.js
@@ -5,24 +5,6 @@ const input = fs
   // .readFileSync(__dirname + "/test.txt", "utf8")
   .split("\r\n");
 
-const sensors = input.map(parseSensor);
-
-let map = new Set();
-// const detectionLine = 10;
-const detectionLine = 2000000;
-
-sensors.forEach((sensor, i) => {
-  countLine(sensor.sensorLocation, sensor.radius, detectionLine);
-});
-
-sensors
-  .filter((sensor) => sensor.beaconLocation[1] === detectionLine)
-  .forEach((sensor) => {
-    map.delete(sensor.beaconLocation[0]);
-  });
-
-console.log(map.size);
-
 function parseSensor(line) {
   const [sensorLocation, beaconLocation] = line
     .match(/x=-?\d+, y=-?\d+/g)
@@ -35,15 +17,79 @@ function getDistance([x1, y1], [x2, y2]) {
   return Math.abs(x1 - x2) + Math.abs(y1 - y2);
 }
 
-function fillCell(x, y) {
-  map.add(x);
+function isIntersectsY([x, y], radius, axis) {
+  return (y <= axis && y + radius >= axis) || (y >= axis && y - radius <= axis);
 }
 
-function countLine([x, y], radius, max) {
-  if ((y <= max && y + radius >= max) || (y >= max && y - radius <= max)) {
-    for (let i = 0; i <= radius - Math.abs(y - max); i++) {
-      fillCell(x + i, max);
-      fillCell(x - i, max);
+function getFilledIntervalY([x, y], radius, axis) {
+  const start = x - radius + Math.abs(y - axis);
+  const end = x + radius - Math.abs(y - axis);
+  return [start, end];
+}
+
+function countCells(intervals) {
+  return intervals.reduce((acc, [start, end]) => acc + end - start, 0);
+}
+
+function compress(intervals) {
+  return intervals
+    .slice()
+    .sort((a, b) => a[0] - b[0])
+    .reduce((acc, [start, end]) => {
+      if (acc.length === 0) {
+        acc.push([start, end]);
+        return acc;
+      }
+
+      const last = acc[acc.length - 1];
+      if (start <= last[1] + 1) {
+        last[1] = Math.max(last[1], end);
+      } else {
+        acc.push([start, end]);
+      }
+
+      return acc;
+    }, []);
+}
+
+function getFilledIntervals(sensors, detectionLine) {
+  const intervals = sensors
+    .filter((sensor) => isIntersectsY(sensor.sensorLocation, sensor.radius, detectionLine))
+    .map((sensor) => getFilledIntervalY(sensor.sensorLocation, sensor.radius, detectionLine));
+
+  return compress(intervals);
+}
+
+function getAvailableIntervals([start, end], filledIntervals) {
+  const availableIntervals = [];
+
+  for (let i = 0; i < filledIntervals.length - 1, start < end; i++) {
+    if (start < filledIntervals[i][0]) {
+      availableIntervals.push([start, filledIntervals[i][0] - 1]);
+      start = filledIntervals[i][1] + 1;
+    } else if (start < filledIntervals[i][1]) {
+      start = filledIntervals[i][1] + 1;
     }
+  }
+
+  if (start < end) availableIntervals.push([start, end]);
+
+  return availableIntervals;
+}
+
+const sensors = input.map(parseSensor);
+
+const filledCells = countCells(getFilledIntervals(sensors, 2000000));
+console.log(filledCells);
+
+// const borders = [0, 20];
+const borders = [0, 4000000];
+
+for (let i = borders[0]; i <= borders[1]; i++) {
+  const filled = getFilledIntervals(sensors, i);
+  const available = getAvailableIntervals(borders, filled);
+  if (available.length > 0) {
+    const frequency = available[0][0] * 4000000 + i;
+    console.log(frequency);
   }
 }
