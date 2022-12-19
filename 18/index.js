111a58f328abf3bf10ebf819006057aeb1a5f013
pezzu <7243796+pezzu@users.noreply.github.com>
2022-12-18 23:44:02 -0500

    Day 18 (refactoring around)

--- 18/index.js
+++ 18/index.js
@@ -9,91 +9,50 @@ const parseCube = (line) => {
 const cubes = input.map(parseCube);
 
 const sides = (cubes) => {
-  return cubes.reduce((sum, cube) => sum + (6 - neighbors(cube, cubes)), 0);
+  return cubes.reduce(
+    (sum, cube) => sum + (6 - neighbors(cube).filter(isExistsIn(cubes)).length),
+    0
+  );
 };
 
-const neighbors = (cube, cubes) => {
-  return cubes.reduce((count, other) => {
-    if (cube === other) return count;
-    return count + (isNeighbor(cube, other) ? 1 : 0);
-  }, 0);
+const neighbors = (cube) => {
+  const [x, y, z] = cube;
+  return [
+    [x + 1, y, z],
+    [x - 1, y, z],
+    [x, y + 1, z],
+    [x, y - 1, z],
+    [x, y, z + 1],
+    [x, y, z - 1],
+  ];
 };
 
-const isNeighbor = (cube, other) => {
-  return cube.map((c, i) => Math.abs(c - other[i])).reduce((sum, diff) => sum + diff, 0) === 1;
+const isExistsIn = (cubes) => (cube) => cubes.some((other) => cube.every((c, i) => c === other[i]));
+
+const isInBox = ([minX, minY, minZ], [maxX, maxY, maxZ]) => (cube) => {
+  const [x, y, z] = cube;
+  return x >= minX && x <= maxX && y >= minY && y <= maxY && z >= minZ && z <= maxZ;
 };
+
+const not = (fn) => (...args) => !fn(...args);
 
 const surface = (cubes) => {
-  return bfs(start(cubes), cubes);
-};
+  const mins = cubes.reduce((box, cube) => cube.map((c, i) => Math.min(box[i], c-1)), [Infinity, Infinity, Infinity]);
+  const maxs = cubes.reduce((box, cube) => cube.map((c, i) => Math.max(box[i], c+1)), [-Infinity, -Infinity, -Infinity]);
 
-const max = (array) => array.reduce((max, item) => Math.max(max, item), -Infinity);
-const min = (array) => array.reduce((min, item) => Math.min(min, item), Infinity);
-const maxX = (cubes) => max(cubes.map((cube) => cube[0])) + 1;
-const minX = (cubes) => min(cubes.map((cube) => cube[0])) - 1;
-const maxY = (cubes) => max(cubes.map((cube) => cube[1])) + 1;
-const minY = (cubes) => min(cubes.map((cube) => cube[1])) - 1;
-const maxZ = (cubes) => max(cubes.map((cube) => cube[2])) + 1;
-const minZ = (cubes) => min(cubes.map((cube) => cube[2])) - 1;
+  const emptyNeighbors = (cube) => neighbors(cube).filter(isInBox(mins, maxs)).filter(not(isExistsIn(cubes)));
+  const solidNeighbors = (cube) => neighbors(cube).filter(isInBox(mins, maxs)).filter(isExistsIn(cubes));
 
-const emptyNeighbors = (cube, cubes) => {
-  const [x, y, z] = cube;
-  return [
-    [x + 1, y, z],
-    [x - 1, y, z],
-    [x, y + 1, z],
-    [x, y - 1, z],
-    [x, y, z + 1],
-    [x, y, z - 1],
-  ]
-    .filter(
-      ([x, y, z]) =>
-        x >= minX(cubes) &&
-        x <= maxX(cubes) &&
-        y >= minY(cubes) &&
-        y <= maxY(cubes) &&
-        z >= minZ(cubes) &&
-        z <= maxZ(cubes)
-    )
-    .filter((cube) => !cubes.some((other) => JSON.stringify(cube) === JSON.stringify(other)));
-};
-
-const start = (cubes) => {
-  return [minX(cubes), minY(cubes), minZ(cubes)];
-};
-
-const nonEmptySides = (cube, cubes) => {
-  const [x, y, z] = cube;
-  return [
-    [x + 1, y, z],
-    [x - 1, y, z],
-    [x, y + 1, z],
-    [x, y - 1, z],
-    [x, y, z + 1],
-    [x, y, z - 1],
-  ]
-    .filter(
-      ([x, y, z]) =>
-        x >= minX(cubes) &&
-        x <= maxX(cubes) &&
-        y >= minY(cubes) &&
-        y <= maxY(cubes) &&
-        z >= minZ(cubes) &&
-        z <= maxZ(cubes)
-    )
-    .filter((cube) => cubes.some((other) => JSON.stringify(cube) === JSON.stringify(other))).length;
-};
-
-const bfs = (start, cubes) => {
   const visited = new Set();
-  const queue = [start];
+  const queue = [mins];
   let count = 0;
+
   while (queue.length) {
     const cube = queue.shift();
     if (visited.has(JSON.stringify(cube))) continue;
     visited.add(JSON.stringify(cube));
-    count += nonEmptySides(cube, cubes);
-    queue.push(...emptyNeighbors(cube, cubes));
+    count += solidNeighbors(cube).length;
+    queue.push(...emptyNeighbors(cube));
   }
   return count;
 };
@@ -124,6 +83,13 @@ console.log(
 );
 
 console.log(sides(cubes));
+
+console.log(
+  surface([
+    [1, 1, 1],
+    [2, 1, 1],
+  ])
+);
 
 console.log(
   surface([
